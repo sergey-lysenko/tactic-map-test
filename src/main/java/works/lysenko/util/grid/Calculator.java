1b71194729a296176edc3f45b52bfc5f2906b858
Sergii Lysenko <sergii@lysenko.works>
2025-12-03 16:03:16 +0200

    Refactor `Calculator` methods to use `Pair` for improved pixel data representation, update related imports, parameters, and add `stamps` handling

--- src/main/java/works/lysenko/util/grid/Calculator.java
+++ src/main/java/works/lysenko/util/grid/Calculator.java
@@ -3,14 +3,19 @@ package works.lysenko.util.grid;
 import org.apache.commons.math3.fraction.Fraction;
 import works.lysenko.util.apis.grid.g._GridCalculator;
 import works.lysenko.util.data.records.Element;
+import works.lysenko.util.data.records.diff.Pair;
 import works.lysenko.util.data.type.Grid;
 import works.lysenko.util.grid.record.rgbc.HSB;
+import works.lysenko.util.prop.grid.Verify;
 
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 import static java.util.Objects.isNull;
+import static works.lysenko.Base.logDebug;
+import static works.lysenko.Base.logTrace;
 import static works.lysenko.util.data.records.RGB24.rgb24;
 
 /**
@@ -44,55 +49,122 @@ public class Calculator implements _GridCalculator {
         return hsb;
     }
 
-    public final Map<Integer, Integer> countPixelsByBrightness(final int fences) {
+    public final Map<Integer, Pair<Integer, String>> countPixelsByBrightness(final int fences) {
 
         return countPixels(HSB::brightness, fences);
     }
 
-    public final Map<Integer, Integer> countPixelsByColor() {
+    public final Map<Integer, Pair<Integer, String>> countPixelsByColor() {
 
-        final Map<Integer, Integer> colorCounts = new HashMap<>(0);
+        logTrace("Processing grid elements");
+        final Map<Integer, Integer> counts = new HashMap<>(0);
+        final Map<Integer, BitSet> bins = new HashMap<>(0);
+        int i = -1;
         for (final Element[] row : grid.getGrid())
-            for (final Element pixel : row)
-                if (null != pixel)
-                    colorCounts.put(pixel.colour(), colorCounts.getOrDefault(pixel.colour(), 0) + 1);
-        return colorCounts;
+            for (final Element pixel : row) {
+                i++;
+                if (null != pixel) {
+                    counts.put(pixel.colour(), counts.getOrDefault(pixel.colour(), 0) + 1);
+                    bins.put(pixel.colour(), bins.getOrDefault(pixel.colour(), new BitSet(1)));
+                    bins.get(pixel.colour()).set(i);
+                }
+            }
+
+        return getIntegerPairMap(counts, bins);
     }
 
-    public final Map<Integer, Integer> countPixelsByHue(final int fences) {
+    /**
+     * Counts the number of pixels based on a given property extracted from the HSB colour representation.
+     *
+     * @param propertyExtractor a function that extracts a property from the HSB colour representation
+     * @param fences            the number of fences levels
+     * @return a results containing the count of pixels for each quantised property value
+     */
+    @SuppressWarnings("MethodWithMultipleReturnPoints")
+    private Map<Integer, Pair<Integer, String>> countPixels(final Function<? super HSB, ? extends Fraction> propertyExtractor, final int fences) {
+
+        if (isNull(propertyExtractor)) return null;
+
+        logTrace("Processing grid elements");
+        final Map<Integer, Integer> counts = new HashMap<>(0);
+        final Map<Integer, BitSet> bins = new HashMap<>(0);
+        int i = -1;
+        for (final HSB[] row : countHSBgrid()) {
+            for (final HSB hsb : row) {
+                i++;
+                if (null != hsb) {
+                    final Fraction property = propertyExtractor.apply(hsb);
+                    final int value = Math.round(property.floatValue() * fences);
+                    counts.put(value, counts.getOrDefault(value, 0) + 1);
+                    bins.put(value, bins.getOrDefault(value, new BitSet(1)));
+                    bins.get(value).set(i);
+                }
+            }
+        }
+
+        return getIntegerPairMap(counts, bins);
+    }
+
+    public final Map<Integer, Pair<Integer, String>> countPixelsByHue(final int fences) {
 
         return countPixels(HSB::hue, fences);
     }
 
-    public final Map<Integer, Integer> countPixelsBySaturation(final int fences) {
+    public final Map<Integer, Pair<Integer, String>> countPixelsBySaturation(final int fences) {
 
         return countPixels(HSB::saturation, fences);
     }
 
     /**
-     * Counts the number of pixels based on a given property extracted from the HSB color representation.
+     * Processes input maps to generate a mapping of integers to pairs, where each pair consists of a count and an encoded
+     * string representation.
      *
-     * @param propertyExtractor a function that extracts a property from the HSB color representation
-     * @param fences            the number of fences levels
-     * @return a map containing the count of pixels for each quantized property value
+     * @param counts a map containing integer keys and their associated counts
+     * @param bins   a map containing integer keys and their associated BitSet values that represent data to encode
+     * @return a map where each key corresponds to an integer from the input maps, and each value is a pair containing the
+     * count from {@code counts} and an encoded string generated
+     * from the associated {@code BitSet} in {@code bins}
      */
-    @SuppressWarnings("MethodWithMultipleReturnPoints")
-    private Map<Integer, Integer> countPixels(final Function<? super HSB, ? extends Fraction> propertyExtractor,
-                                              final int fences) {
+    private static Map<Integer, Pair<Integer, String>> getIntegerPairMap(final Map<Integer, Integer> counts,
+                                                                         final Map<Integer, ? extends BitSet> bins) {
 
-        if (isNull(propertyExtractor)) return null;
-        final HSB[][] hsbGrid = countHSBgrid();
-        final Map<Integer, Integer> counts = new HashMap<>(0);
+        final Map<Integer, Pair<Integer, String>> result = new HashMap<>(counts.size());
 
-        for (final HSB[] row : hsbGrid) {
-            for (final HSB hsb : row) {
-                if (null != hsb) {
-                    final Fraction property = propertyExtractor.apply(hsb);
-                    final int value = Math.round(property.floatValue() * fences);
-                    counts.put(value, counts.getOrDefault(value, 0) + 1);
+        logTrace("Calculating color stamps");
+        final java.util.Base64.Encoder encoder = java.util.Base64.getUrlEncoder().withoutPadding();
+        for (final Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            final BitSet bin = bins.get(entry.getKey());
+            final StringBuilder encoded = new StringBuilder();
+
+            if (Verify.stamps) {
+
+                int start = bin.nextSetBit(0);
+                while (start >= 0) {
+                    int end = start;
+                    int next = bin.nextSetBit(end + 1);
+
+                    while (next >= 0 && (next - end) < 8) {
+                        end = next;
+                        next = bin.nextSetBit(end + 1);
+                    }
+
+                    if (0 < encoded.length()) {
+                        encoded.append("~");
+                    }
+
+                    final BitSet slice = bin.get(start, end + 1);
+                    final byte[] bytes = slice.toByteArray();
+                    final String b64 = encoder.encodeToString(bytes);
+
+                    encoded.append(Integer.toString(start, 36)).append("=").append(b64);
+                    start = next;
                 }
             }
+
+            result.put(entry.getKey(), new Pair<>(entry.getValue(),
+                    encoded.length() > 0 ? encoded.toString() : ""));
         }
-        return counts;
+
+        return result;
     }
 }
