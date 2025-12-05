e06d7745560edf634f42e4dde05458e7c1967d53
Sergii Lysenko <sergii@lysenko.works>
2025-12-05 11:19:48 +0200

    Add 15 new classes under `tacticmap.tree.root` extending `Node` with unimplemented `action` method.

---
+++ src/main/java/tacticmap/tree/root/Ruler.java
@@ -0,0 +1,18 @@
+package tacticmap.tree.root;
+
+import works.lysenko.tree.base.Node;
+import works.lysenko.util.apis.exception.checked.SafeguardException;
+
+import static works.lysenko.util.func.core.Assertions.notImplemented;
+
+@SuppressWarnings({"unused", "MissingJavadoc"})
+public class Ruler extends Node {
+
+    @Override
+    public final void action() throws SafeguardException {
+
+        notImplemented(true);
+    }
+}
+
+
